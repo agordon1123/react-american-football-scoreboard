7756fbfe24592d9cd68ffd91a2d7889682059395
Alex <agordon1123@gmail.com>
2019-07-15 18:52:16 -0400

    individual components created

--- src/App.js
+++ src/App.js
@@ -1,43 +1,13 @@
-//TODO: STEP 1 - Import the useState hook.
-import React, { useState } from "react";
+import React from "react";
+import Scoreboard from "./Scoreboard";
+import Buttons from "./Buttons";
 import "./App.css";
-import BottomRow from "./BottomRow";
 
 function App() {
-  //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
-  const [homeScore, homeScoreCount] = useState(true);
-  const [awayScore, awayScoreCount] = useState(true);
-
   return (
     <div className="container">
-      <section className="scoreboard">
-        <div className="topRow">
-          <div className="home">
-            <h2 className="home__name">Lions</h2>
+      <Scoreboard />
 
-            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-
-            <div className="home__score">{homeScore}</div>
-          </div>
-          <div className="timer">00:03</div>
-          <div className="away">
-            <h2 className="away__name">Tigers</h2>
-            <div className="away__score">{awayScore}</div>
-          </div>
-        </div>
-        <BottomRow />
-      </section>
-      <section className="buttons">
-        <div className="homeButtons">
-          {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button onClick={ () => homeScoreCount(homeScore + 7)} className="homeButtons__touchdown">Home Touchdown</button>
-          <button onClick={() => homeScoreCount(homeScore + 3)} className="homeButtons__fieldGoal">Home Field Goal</button>
-        </div>
-        <div className="awayButtons">
-          <button onClick={() => awayScoreCount(awayScore + 7)} className="awayButtons__touchdown">Away Touchdown</button>
-          <button onClick={() => awayScoreCount(awayScore + 3)} className="awayButtons__fieldGoal">Away Field Goal</button>
-        </div>
-      </section>
     </div>
   );
 }
